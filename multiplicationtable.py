7807c455bf55ce8d2fe35750b5a7e2083e3bdc87
Pippcicle <pippaamsmith@gmail.com>
2025-02-20 18:43:24 +0000

    Add files via upload

--- multiplicationtable.py
+++ multiplicationtable.py
@@ -16,12 +16,29 @@ drop_down = Combobox(window, textvariable = num, width = 5, state = 'readonly')
 drop_down['values'] = tuple(range(101))
 drop_down.grid( row = 1, column = 1)
 
+#radiobuttons creation
+endval  = IntVar()
+r10 = Radiobutton(window, text = '10', variable = endval, value = 10)
+r20 = Radiobutton(window, text = '20', variable = endval, value = 20)
+r30 = Radiobutton(window, text = '30', variable = endval, value = 30)
+endval.set(10)
 
+r10.grid( row = 1, column = 2)
+r20.grid( row = 2, column = 2, padx = 30)
+r30.grid( row = 3, column = 2, padx = 30)
 
+def generate_table():
+    tables = ""
 
+    for i in range(1, endval.get() + 1):
+        mult = num.get() * i
+        tables += str(num.get()) + " X " + str(i) + " = " + str(mult) + "\n"
+    table.configure(text = tables)
 
+generate = Button( window, text = 'generate', command = generate_table)
+generate.grid( row = 4, column = 1)
 
-
-
+table = Label(window, anchor = 'center')
+table.grid( row = 5, column = 1, pady = 25)
 
 window.mainloop()
